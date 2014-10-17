e21d1681eb92876f55573e0d7b5ebf20eba79949
Ngoc Dao <ngocdaothanh@gmail.com>
2014-10-17 18:02:18 +0900

    Add more tests

--- src/test/scala/jauter/RoutingSpec.scala
+++ src/test/scala/jauter/RoutingSpec.scala
@@ -32,4 +32,19 @@ class RoutingSpec extends FlatSpec with Matchers {
     val routed = router.route("/noexist")
     (routed == null) should be (true)
   }
+
+  "A router" should "route subclasses" in {
+    trait Action
+    class Index extends Action
+    class Show  extends Action
+
+    val router = new Router[Class[_ <: Action]]
+    router.pattern("/articles",     classOf[Index])
+    router.pattern("/articles/:id", classOf[Show])
+
+    val routed1 = router.route("/articles")
+    val routed2 = router.route("/articles/123")
+    routed1.target should be (classOf[Index])
+    routed2.target should be (classOf[Show])
+  }
 }
